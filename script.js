5057ec3c2b437cae7a99032c7483cc116ca510f8
topzkk <156640330+topzkk@users.noreply.github.com>
2025-02-14 04:05:53 +0700

    123

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     const noButton = document.getElementById("no");
 
-    noButton.addEventListener("mouseover", () => {
-        const x = Math.random() * window.innerWidth - noButton.clientWidth;
-        const y = Math.random() * window.innerHeight - noButton.clientHeight;
+    function moveButton() {
+        const x = Math.random() * (window.innerWidth - noButton.clientWidth);
+        const y = Math.random() * (window.innerHeight - noButton.clientHeight);
 
         noButton.style.position = "absolute";
         noButton.style.left = `${x}px`;
         noButton.style.top = `${y}px`;
-    });
+    }
+
+    noButton.addEventListener("mouseover", moveButton);
+    
+    noButton.addEventListener("click", moveButton);
 });
 
+
 function nextPage() {
-    window.location.href = "yes.html"; // เปลี่ยนหน้าไปยัง yes.html
+    window.location.href = "yes.html"; 
 }
